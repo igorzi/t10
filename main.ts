7ca40fbe7649946e114d89bd0d5a865675264076
Igor Zinkovsky <igor@deno.com>
2023-06-07 17:23:07 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
-import { serve } from "https://deno.land/std@0.11177.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.11177.0/http/ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssserver.ts";
 
 serve((req: Request) => new Response("Hello World100"));
